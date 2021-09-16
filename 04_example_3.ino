d1f40b3fa46a4ec567b33baac2186389f872f5d0
hwstage <59343778+hwstage@users.noreply.github.com>
2021-09-16 12:30:13 +0900

    과제수정

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -1,5 +1,6 @@
 #define PIN_LED 13
 unsigned int count, toggle;
+
 void setup() {
   pinMode(PIN_LED, OUTPUT);
   Serial.begin(115200); // Initialize serial port
@@ -14,7 +15,7 @@ void setup() {
 
 void loop() {
   Serial.println(++count);
-  toggle = toggle_state(toggle); //toggle LED value.
+  toggle = toggle_state(!toggle); //toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); //wait for 1,000 milliseconds
 
